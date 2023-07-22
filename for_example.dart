f193ba59c99a180fe5f584ea86e44b3e7accee5b
tjlee0821 <teo0821@gmail.com>
2023-07-22 10:25:57 +0900

    for_exception_examples

--- for_example.dart
+++ for_example.dart
@@ -2,7 +2,25 @@ void main(){
   List<int> indexes= [0,1,2,3,4,5,6,];
 
   for (final index in indexes){
-    print("running form indexes $index");
+//    print("running form indexes $index");
   }
+
+  bool isRunning = true;
+  int count = 0;
+  while (isRunning){
+    if (count >=5) {isRunning = false; break;}
+    count ++;
+    //print("while is Running");
+  }
+
+  int num = 0;
+  do {
+      num ++;
+
+      if(num == 4){
+        continue;
+      }
+      print("Do while is Running $num");
+  } while ( num < 10 );
 
 }
