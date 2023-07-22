b64eeadc27034bec4bcac7736e6e3a548d163a6a
tjlee0821 <teo0821@gmail.com>
2023-07-22 10:41:23 +0900

    exceptionExample_Try

--- exception_example.dart
+++ exception_example.dart
@@ -1,4 +1,12 @@
 void main(){
   int num1 = 10;
-  print(10 /0);
+  double quoti = 0;
+  try{
+    quoti = num1/0;
+    print(quoti);
+    print(10 ~/ 0);
+  }catch(e){
+    print(e);
+    print("Error Occurs");
+  }
 }
