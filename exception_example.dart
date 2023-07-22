710b5fde9fcf35c77a91606a13455584fab00d67
tjlee0821 <teo0821@gmail.com>
2023-07-22 10:58:28 +0900

    exception_continued

--- exception_example.dart
+++ exception_example.dart
@@ -1,12 +1,34 @@
-void main(){
+void main() {
   int num1 = 10;
   double quoti = 0;
-  try{
-    quoti = num1/0;
+  try {
+    quoti = num1 / 0;
     print(quoti);
     print(10 ~/ 0);
-  }catch(e){
+  } catch (e) {
     print(e);
-    print("Error Occurs");
+    print("Error Occurs ~~~~!!!!");
+  } finally {
+    /// 예외가 발생 했던, 하지 않았던, try 문/ catch 문 이후에 실행하고자 하는 코드 
+    /// 
+    print ("예외처리 문이 지나쳤습니다");
   }
+
+  try{
+   // print(10 ~/ 0);
+    throw Exception( 'Unknown Error!!!!');
+  }
+  on IntegerDivisionByZeroException catch(e, s){
+    print(e);
+    print(s);
+  }
+  on TypeError catch(e, s){
+    print("TypeError Null 값이 참조되었씁니다.");
+    print(s);
+  } catch (e,s){
+    print('모르는 에러가 감지되었습니다.');
+    print(s);
+    rethrow;
+  }
+
 }
