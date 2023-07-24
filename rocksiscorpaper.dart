1cfc816fc5b8dc72ba1b7c2bf96fd505bb935db0
tjlee0821 <teo0821@gmail.com>
2023-07-24 10:40:10 +0900

    noHardCoding

--- rocksiscorpaper.dart
+++ rocksiscorpaper.dart
@@ -1,9 +1,9 @@
 import 'dart:convert';
 import 'dart:io';
 import 'dart:math';
-// 터미널에서 시켜야 한다
-void main(){
 
+// 터미널에서 시켜야 한다fast
+void main() {
   ////1. 유저에게 어떤것을 낼것인지
   ////2. 컴퓨터가 낼 타입이 어떤것인지
   ////3. 유저의 타입과 컴퓨터 타입에 대한 결과를 계산할 함수
@@ -11,10 +11,11 @@ void main(){
   print('rock, sicsor, paper 중 하나를 정해서 입력해주세요');
   //String userInput = stdin.readLineSync(encoding: utf8) ?? 'Error'; // standard input
   //String? userInput = stdin.readLineSync(encoding: utf8); // standard input
-  final String userInput = stdin.readLineSync(encoding: utf8) ?? 'Error'; // standard input
+  final String userInput =
+      stdin.readLineSync(encoding: utf8) ?? 'Error'; // standard input
   print(userInput);
 
-  const selectList = ['rock','sicsor','paper'];
+  const selectList = ['rock', 'sicsor', 'paper'];
 
   final cpuInput = selectList[Random().nextInt(3)];
 
@@ -23,34 +24,37 @@ void main(){
   print(getResult(userInput, cpuInput));
 }
 
-String getResult(String userInput, String cpuInput){
+String getResult(String userInput, String cpuInput) {
   String result = '';
-  switch(userInput){
-    
+  String usrWin = 'User Win';
+  String cpuWin = 'Cpu Win';
+  String noWin = 'Draw';
+  
+  switch (userInput) {
     case 'rock':
-      if(cpuInput == 'rock'){
-        result = 'Draw';
-      }else if(cpuInput == 'sicsor'){
-        result =  'User Win';
-      }else{
-        result =  'Cpu Win';
-      } 
+      if (cpuInput == 'rock') {
+        result = noWin;
+      } else if (cpuInput == 'sicsor') {
+        result = usrWin;
+      } else {
+        result = cpuWin;
+      }
     case 'sicsor':
-      if(cpuInput == 'rock'){
-        result =  'CPU win';
-      }else if(cpuInput == 'sicsor'){
-        result =  'Draw';
-      }else{
-        result =  'User Win';
-      } 
+      if (cpuInput == 'rock') {
+        result = cpuWin;
+      } else if (cpuInput == 'sicsor') {
+        result = noWin;
+      } else {
+        result = usrWin;
+      }
     case 'paper':
-      if(cpuInput == 'rock'){
-        result =  'User Win';
-      }else if(cpuInput == 'sicsor'){
-        result =  'Cpu Win';
-      }else{
-        result =  'Draw';
-      }      
+      if (cpuInput == 'rock') {
+        result = usrWin;
+      } else if (cpuInput == 'sicsor') {
+        result = cpuWin;
+      } else {
+        result = noWin;
+      }
   }
   return result;
 }
